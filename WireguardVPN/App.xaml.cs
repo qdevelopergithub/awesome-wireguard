eaddccabe366bb5062dcd3682e98c23ef8d2dba9
sahilk <sahil@fldata.com>
2024-07-01 17:20:37 +0530

    Work in progress

---
+++ WireguardVPN/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using Com.Wireguard.Android;
+using Com.Wireguard.Config;
+using Com.Wireguard.Crypto;
+using Com.Wireguard.Util;
+
+namespace WireguardVPN
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ WireguardVPN/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace WireguardVPN
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
